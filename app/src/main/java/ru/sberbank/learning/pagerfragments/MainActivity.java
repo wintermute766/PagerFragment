17c2886c66395c10377661b00504de53ea793fe7
Eugene K <player766@gmail.com>
2017-05-04 18:46:18 +0200

    gradle

--- app/src/main/java/ru/sberbank/learning/pagerfragments/MainActivity.java
+++ app/src/main/java/ru/sberbank/learning/pagerfragments/MainActivity.java
@@ -1,9 +1,9 @@
 package ru.sberbank.learning.pagerfragments;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
