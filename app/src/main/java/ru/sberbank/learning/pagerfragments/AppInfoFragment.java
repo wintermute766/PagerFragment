f0d95b70bc73fdf859ad5482789d48188f4020ed
Eugene K <player766@gmail.com>
2017-05-04 18:53:13 +0200

    temp0

---
+++ app/src/main/java/ru/sberbank/learning/pagerfragments/AppInfoFragment.java
@@ -0,0 +1,8 @@
+package ru.sberbank.learning.pagerfragments;
+
+/**
+ * Created by user10 on 04.05.2017.
+ */
+
+public class AppInfoFragment {
+}
---
+++ app/src/main/java/ru/sberbank/learning/pagerfragments/AppsPagerAdapter.java
@@ -0,0 +1,10 @@
+package ru.sberbank.learning.pagerfragments;
+
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by user10 on 04.05.2017.
+ */
+
+public class AppsPagerAdapter extends FragmentStatePagerAdapter {
+}
